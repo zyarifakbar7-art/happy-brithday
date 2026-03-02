04d78397f32e11fdedc489a5028beafb65d71f60
zyarifakbar7-art <zyarifakbar7@gmail.com>
2026-03-02 21:16:54 +0700

    InteractionFlow.tsx

--- components/InteractionFlow.tsx
+++ components/InteractionFlow.tsx
@@ -340,7 +340,7 @@ const LoveMeterStep = ({ onComplete }: { onComplete: () => void }) => {
 
 // --- Step 4: Typewriter ---
 const TypewriterStep = ({ onComplete }: { onComplete: () => void }) => {
-    const text = "Happy Valentine!!!!";
+    const text = "Happy Birthday Kisaaa🎉🎂🥳";
     const [displayedText, setDisplayedText] = useState("");
     const [isDeleting, setIsDeleting] = useState(false);
 
